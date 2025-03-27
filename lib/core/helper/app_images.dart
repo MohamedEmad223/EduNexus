ef12eda928cab1a639df3d5dc11e9e7cef52a740
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-03-27 22:21:30 +0200

    add font to assets folder

--- lib/core/helper/app_images.dart
+++ lib/core/helper/app_images.dart
@@ -1,6 +1,8 @@
 class AppImages {
   /// Path to the splash logo
   static const String splashLogo = 'assets/images/splash/EduNEXUS.svg';
+
   /// Path to the onboarding image
-  static const String onBoardingImage = 'assets/images/onboarding/onboarding.svg';
+  static const String onBoardingImage =
+      'assets/images/onboarding/onBoarding.svg';
 }
--- lib/feature/on_boarding/on_boarding.dart
+++ lib/feature/on_boarding/on_boarding.dart
@@ -1,13 +1,63 @@
+import 'package:edunexus/core/helper/app_images.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: SafeArea(child: Column(children: [
-          
-      ],
-    )));
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(height: 50),
+              SvgPicture.asset(AppImages.onBoardingImage),
+              Text(
+                "Find yourself by doing\n whatever you do",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 100),
+              SizedBox(
+                height: 55,
+                width: double.infinity,
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Color(0xffFF6636),
+                  textColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Text("Login"),
+                ),
+              ),
+              const SizedBox(height: 50),
+              SizedBox(
+                height: 55,
+                width: double.infinity,
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.white,
+                  textColor: Color(0xffFF6636),
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(color: Color(0xffFF6636), width: 2.5),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Text("Register"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
