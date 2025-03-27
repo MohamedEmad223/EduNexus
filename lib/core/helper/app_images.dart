0a245bd39dedcc5b94b7f3ec70054154fa9962c5
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-03-27 22:00:00 +0200

    add onBoardingImage path to AppImages

--- lib/core/helper/app_images.dart
+++ lib/core/helper/app_images.dart
@@ -1,4 +1,6 @@
 class AppImages {
   /// Path to the splash logo
   static const String splashLogo = 'assets/images/splash/EduNEXUS.svg';
+  /// Path to the onboarding image
+  static const String onBoardingImage = 'assets/images/onboarding/onboarding.svg';
 }
