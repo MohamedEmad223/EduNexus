10dacd23e5b7aba236aaa24690ee08f0fe632b8b
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-03-27 20:25:24 +0200

    add edunuexus app file

---
+++ lib/edu_neuxus.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EduNeuxus extends StatelessWidget {
+  const EduNeuxus({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: const HomeScreen());
+  }
+}
