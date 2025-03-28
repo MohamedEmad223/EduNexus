9d964f8d36045e666f0177026a4645e095758dd1
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-03-28 23:26:05 +0200

    add the image of login screen

--- lib/feature/auth/login/views/screens/login_screen.dart
+++ lib/feature/auth/login/views/screens/login_screen.dart
@@ -1,10 +1,28 @@
+import 'package:edunexus/core/helper/app_images.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Login Screen')));
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0.h, vertical: 70.0.w),
+          child: Column(
+            children: [
+              Center(
+                child: Image.asset(
+                  AppImages.splashLogo,
+                  alignment: Alignment.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
