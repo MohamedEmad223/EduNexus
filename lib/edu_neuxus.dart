661ca4cafa58b0956568c9946385036c7b922fde
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-04-05 08:36:15 +0200

    add them to eduneuxes

--- lib/edu_neuxus.dart
+++ lib/edu_neuxus.dart
@@ -1,5 +1,6 @@
 import 'package:edunexus/core/routes/app_rotes.dart';
 import 'package:edunexus/core/routes/routes.dart';
+import 'package:edunexus/core/theme/app_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -14,6 +15,17 @@ class EduNeuxus extends StatelessWidget {
       ensureScreenSize: true,
 
       child: MaterialApp(
+        theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            elevation: 0,
+            color: Colors.white,
+            surfaceTintColor: Colors.white,
+            scrolledUnderElevation: 0,
+            iconTheme: IconThemeData(
+              color: AppColor.scaffoldBackgroundColor,
+              size: 28.r,
+            ),
+          ),),
         debugShowCheckedModeBanner: false,
         onGenerateRoute: appRoutes.generateRoute,
         initialRoute: Routes.botNavBar,
