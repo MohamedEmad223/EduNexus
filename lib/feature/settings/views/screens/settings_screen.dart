20e008a540e22305be451cb6a6e1ca71061716e7
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-04-05 11:30:28 +0200

    add EditRowWidget

--- lib/feature/settings/views/screens/settings_screen.dart
+++ lib/feature/settings/views/screens/settings_screen.dart
@@ -1,10 +1,9 @@
 import 'package:edunexus/core/helper/app_images.dart';
-import 'package:edunexus/core/theme/app_color.dart';
 import 'package:edunexus/core/theme/app_text_style.dart';
+import 'package:edunexus/feature/settings/views/widgets/edit_row_widget.dart';
 import 'package:edunexus/feature/settings/views/widgets/information_container_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -21,31 +20,21 @@ class SettingsScreen extends StatelessWidget {
         children: [
           InformationContainerSettings(),
           SizedBox(height: 50.h),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.h),
-            child: Container(
-              padding: EdgeInsets.all(20.w),
-              decoration: BoxDecoration(
-                color: AppColor.backGroundColor,
-                borderRadius: BorderRadius.circular(25.r),
-              ),
-
-              height: 65.h,
-              width: double.infinity,
-              child: Row(
-                children: [
-                  SvgPicture.asset(AppImages.editIcon),
-                  SizedBox(width: 10.w),
-                  Text(
-                    'Edit Profile',
-                    style: AppTextStyle.poppins14w300threedBlackColor,
-                  ),
-                  Spacer(),
-                  SvgPicture.asset(AppImages.arrowIcon),
-                ],
-              ),
-            ),
+          EditRowWidget(text: 'Edit Profile', imagePath: AppImages.editIcon),
+          SizedBox(height: 20.h),
+          EditRowWidget(
+            text: 'Courses History',
+            imagePath: AppImages.coursesHistoryIcon,
           ),
+          SizedBox(height: 20.h),
+          EditRowWidget(text: 'Courses', imagePath: AppImages.coursesIcon),
+          SizedBox(height: 20.h),
+          EditRowWidget(
+            text: 'Change Password',
+            imagePath: AppImages.changePasswordIcon,
+          ),
+          SizedBox(height: 20.h),
+          EditRowWidget(text: 'log Out', imagePath: AppImages.logOutIcon),
         ],
       ),
     );
