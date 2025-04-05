d416f0b32129f687bc845f233d6c5428de527c76
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-04-05 08:56:41 +0200

    add InformationOfUserWidget

--- lib/feature/home/view/screens/home_screen.dart
+++ lib/feature/home/view/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:edunexus/core/helper/app_images.dart';
-import 'package:edunexus/core/theme/app_text_style.dart';
+import 'package:edunexus/feature/home/view/widgets/information_of_user_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -16,34 +16,7 @@ class HomeScreen extends StatelessWidget {
           padding: EdgeInsets.symmetric(horizontal: 20.h, vertical: 20.w),
           child: Column(
             children: [
-              Row(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Hi! Ali',
-                        style: AppTextStyle.poppins16w400blackColor,
-                      ),
-                      Text(
-                        'What do you wanna learn today?',
-                        style: AppTextStyle.poppins14w300blackColor,
-                      ),
-                    ],
-                  ),
-                  const Spacer(),
-                  Container(
-                    height: 50.h,
-                    width: 50.w,
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(25.r),
-                    ),
-                    child: Image.asset(AppImages.personIcon),
-                  ),
-                ],
-              ),
-
+              InformationOfUserWidget(),
               SizedBox(height: 50.h),
               Image.asset(AppImages.learn),
             ],
