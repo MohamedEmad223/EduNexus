fdcd6e64c8c7c221ee6b8a83b5280730da4f23fa
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-04-10 14:27:54 +0200

    add LearnedTodayContainer

--- lib/feature/courses/views/screens/courses_screen.dart
+++ lib/feature/courses/views/screens/courses_screen.dart
@@ -1,4 +1,8 @@
+import 'package:edunexus/core/theme/app_color.dart';
+import 'package:edunexus/core/theme/app_text_style.dart';
+import 'package:edunexus/feature/courses/views/widgets/learned_today_container.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CoursesScreen extends StatelessWidget {
   const CoursesScreen({super.key});
@@ -6,14 +10,22 @@ class CoursesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColor.backGroundColor,
       appBar: AppBar(
-        title: const Text('Courses'),
+        backgroundColor: AppColor.backGroundColor,
+        elevation: 0,
+        title: Text(
+          'My Courses',
+          style: AppTextStyle.poppins18w500lightBlackColor,
+        ),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'Courses Screen',
-          style: TextStyle(fontSize: 24),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 15.h),
+        child: Column(
+          children: [
+            LearnedTodayContainer()
+          ],
         ),
       ),
     );
