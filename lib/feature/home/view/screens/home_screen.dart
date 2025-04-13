31fad24488f336a181557a6a4ca903c6802f7a79
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-04-14 00:32:43 +0200

    finish feature/notifications

--- lib/feature/home/view/screens/home_screen.dart
+++ lib/feature/home/view/screens/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:edunexus/core/helper/app_images.dart';
+import 'package:edunexus/core/routes/routes.dart';
 import 'package:edunexus/core/theme/app_color.dart';
 import 'package:edunexus/core/theme/app_text_style.dart';
 import 'package:edunexus/feature/home/view/widgets/information_of_user_widget.dart';
 import 'package:edunexus/feature/home/view/widgets/list_view_categories_widgets.dart';
+import 'package:edunexus/feature/notification/presentation/screens/notificatios_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -20,6 +22,18 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => NotificatiosScreen(),
+                    ),
+                  );
+                },
+                child: Image.asset(AppImages.notificationIcon),
+              ),
+              SizedBox(height: 20.h),
               InformationOfUserWidget(),
               SizedBox(height: 50.h),
               Image.asset(AppImages.learn),
@@ -32,8 +46,8 @@ class HomeScreen extends StatelessWidget {
               ListViewCategoriesWidgets(),
               SizedBox(height: 20.h),
               Text('Top Courses', style: AppTextStyle.poppins20BoldblackColor),
-              // TODO: i will add courses card here when they finidh designed them
 
+              // TODO: i will add courses card here when they finidh designed them
             ],
           ),
         ),
