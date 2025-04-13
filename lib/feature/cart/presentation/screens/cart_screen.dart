c3928489ffa3200c7acd7228f3817c1657a6aabe
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-04-13 07:15:57 +0200

    added cart images to assets

--- lib/feature/cart/presentation/screens/cart_screen.dart
+++ lib/feature/cart/presentation/screens/cart_screen.dart
@@ -1,4 +1,7 @@
+import 'package:edunexus/core/helper/app_images.dart';
+import 'package:edunexus/core/theme/app_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -7,11 +10,22 @@ class CartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Cart'), centerTitle: true),
-      body: Column(
-        children: [
-          
-        ],
-      )
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 15.h),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 116.h,
+              color: AppColor.primaryColor,
+              child: Row(children: [Column(children: [
+        Image.asset(AppImages.)
+                    ],
+                  )]),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
