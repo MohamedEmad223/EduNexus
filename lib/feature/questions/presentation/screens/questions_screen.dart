571da1599058beb8875216a19c2d0022c591dab7
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-04-22 12:15:51 +0200

    add AnswerContainerWidgets

--- lib/feature/questions/presentation/screens/questions_screen.dart
+++ lib/feature/questions/presentation/screens/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:edunexus/core/helper/app_images.dart';
 import 'package:edunexus/core/theme/app_color.dart';
 import 'package:edunexus/core/theme/app_text_style.dart';
+import 'package:edunexus/feature/questions/presentation/widgets/answer_container_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -19,10 +20,58 @@ class QuestionsScreen extends StatelessWidget {
           child: Column(
             children: [
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text('Quize', style: AppTextStyle.poppins18secondBlackColor),
-                  SvgPicture.asset(AppImages.)
+                  CircleAvatar(
+                    radius: 15.r,
+                    backgroundColor: AppColor.whiteColor,
+                    child: SvgPicture.asset(AppImages.cancelIcone),
+                  ),
                 ],
+              ),
+              SizedBox(height: 50.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: 300.w,
+                    child: LinearProgressIndicator(
+                      value: 0.1,
+                      backgroundColor: AppColor.whiteColor,
+                      color: AppColor.primaryColor,
+                      minHeight: 5.h,
+                      borderRadius: BorderRadius.circular(10.r),
+                    ),
+                  ),
+                  Text("5/1"),
+                ],
+              ),
+              SizedBox(height: 50.h),
+
+              Text(
+                'Which of the following is not a principle of Object-Oriented Programming?',
+                style: AppTextStyle.poppins18w500lightBlackColor,
+              ),
+              SizedBox(height: 20.h),
+              AnswerContainerWidgets(
+                answerOption: 'A',
+                answerText: 'Abstraction',
+              ),
+              SizedBox(height: 20.h),
+              AnswerContainerWidgets(
+                answerOption: 'B',
+                answerText: 'Polymorphism',
+              ),
+              SizedBox(height: 20.h),
+              AnswerContainerWidgets(
+                answerOption: 'C',
+                answerText: 'Inheritance',
+              ),
+              SizedBox(height: 20.h),
+              AnswerContainerWidgets(
+                answerOption: 'D',
+                answerText: 'Compilation',
               ),
             ],
           ),
---
+++ lib/feature/questions/presentation/widgets/answer_container_widgets.dart
@@ -0,0 +1,50 @@
+import 'package:edunexus/core/theme/app_color.dart';
+import 'package:edunexus/core/theme/app_text_style.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AnswerContainerWidgets extends StatelessWidget {
+  const AnswerContainerWidgets({
+    super.key,
+    required this.answerText,
+    required this.answerOption,
+  });
+
+  final String answerText;
+  final String answerOption;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15.w),
+      width: double.infinity,
+      height: 60.h,
+      decoration: BoxDecoration(
+        color: AppColor.whiteColor,
+        borderRadius: BorderRadius.circular(5.r),
+        boxShadow: [
+          BoxShadow(
+            color: AppColor.lightGreyColor.withOpacity(0.5),
+            blurRadius: 5.r,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 15.r,
+            backgroundColor: AppColor.scaffoldBackgroundColor,
+            child: Text(
+              answerOption,
+              style: AppTextStyle.poppins20BoldblackColor,
+            ),
+          ),
+          SizedBox(width: 10.w),
+          Text(answerText, style: AppTextStyle.poppins16w400blackColor),
+          
+        ],
+      ),
+    );
+  }
+}
