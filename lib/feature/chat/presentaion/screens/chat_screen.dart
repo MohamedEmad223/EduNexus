cd7bd2f2d348204cb9e04807035b3929c34d5c27
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-04-22 14:50:00 +0200

    add ChatScreen to feature

---
+++ lib/feature/chat/presentaion/screens/chat_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat Screen'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          'Chat Screen Content',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
