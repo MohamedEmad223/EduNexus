5c587c64e6407109f64b8b2c9e19cbfc9d1b0240
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-04-24 13:40:05 +0200

    add formKey,emailController and passwordController to cubit

--- lib/feature/auth/register/cubit/cubit/signup_cubit.dart
+++ lib/feature/auth/register/cubit/cubit/signup_cubit.dart
@@ -1,11 +1,16 @@
 import 'package:edunexus/feature/auth/register/data/model/sign_up_model.dart';
 import 'package:edunexus/feature/auth/register/data/repo/sign_up_repository.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'signup_state.dart';
 
 class SignupCubit extends Cubit<SignupState> {
   SignupCubit(this.signUpRepository) : super(SignupInitial());
+
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   final SignUpRepository signUpRepository;
 
