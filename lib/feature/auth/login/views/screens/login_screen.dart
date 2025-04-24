8afda0fddb604b7cfac48edad99ae8202f59735b
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-04-24 14:59:44 +0200

    add FormOfLoginScreen

--- lib/feature/auth/login/views/screens/login_screen.dart
+++ lib/feature/auth/login/views/screens/login_screen.dart
@@ -1,10 +1,5 @@
 import 'package:edunexus/core/helper/app_images.dart';
-import 'package:edunexus/core/routes/routes.dart';
-import 'package:edunexus/core/theme/app_color.dart';
-import 'package:edunexus/core/theme/app_text_style.dart';
-import 'package:edunexus/core/widgets/text_form_feild.dart';
-import 'package:edunexus/feature/auth/login/views/widgets/do_not_have_an_account.dart';
-import 'package:edunexus/feature/auth/login/views/widgets/lable_text.dart';
+import 'package:edunexus/feature/auth/login/views/widgets/form_of_login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -28,69 +23,7 @@ class LoginScreen extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 80.h),
-              LableText(text: "Email"),
-              SizedBox(height: 10.h),
-              AppTextFormField(
-                hintText: "Enter your email",
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Please enter your email";
-                  } else if (!RegExp(
-                    r"^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+",
-                  ).hasMatch(value)) {
-                    return "Please enter a valid email address";
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 20.h),
-              LableText(text: "Password"),
-              SizedBox(height: 10.h),
-              AppTextFormField(
-                hintText: "Enter your password",
-                isObscureText: true,
-                suffixIcon: const Icon(Icons.visibility_off),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Please enter your password";
-                  } else if (value.length < 6) {
-                    return "Password must be at least 6 characters long";
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 80.h),
-              SizedBox(
-                height: 55.h,
-                width: double.infinity,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamedAndRemoveUntil(
-                      context,
-                      Routes.botNavBar,
-                      (route) => false,
-                    );
-                  },
-                  color: AppColor.primaryColor,
-                  textColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.sp),
-                  ),
-                  child: Text(
-                    "Login",
-                    style: AppTextStyle.poppins14BoldwhiteColor,
-                  ),
-                ),
-              ),
-              DoNotHaveAnAccount(
-                text: "Register",
-                onPressed:
-                    () => Navigator.pushNamedAndRemoveUntil(
-                      context,
-                      '/register',
-                      (route) => false,
-                    ),
-              ),
+              FormOfLoginScreen(),
             ],
           ),
         ),
---
+++ lib/feature/auth/login/views/widgets/form_of_login_screen.dart
@@ -0,0 +1,82 @@
+import 'package:edunexus/core/routes/routes.dart';
+import 'package:edunexus/core/theme/app_color.dart';
+import 'package:edunexus/core/theme/app_text_style.dart';
+import 'package:edunexus/core/widgets/text_form_feild.dart';
+import 'package:edunexus/feature/auth/login/views/widgets/do_not_have_an_account.dart';
+import 'package:edunexus/feature/auth/login/views/widgets/lable_text.dart'
+    show LableText;
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class FormOfLoginScreen extends StatelessWidget {
+  const FormOfLoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        LableText(text: "Email"),
+        SizedBox(height: 10.h),
+        AppTextFormField(
+          hintText: "Enter your email",
+          validator: (value) {
+            if (value!.isEmpty) {
+              return "Please enter your email";
+            } else if (!RegExp(
+              r"^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+",
+            ).hasMatch(value)) {
+              return "Please enter a valid email address";
+            }
+            return null;
+          },
+        ),
+        SizedBox(height: 20.h),
+        LableText(text: "Password"),
+        SizedBox(height: 10.h),
+        AppTextFormField(
+          hintText: "Enter your password",
+          isObscureText: true,
+          suffixIcon: const Icon(Icons.visibility_off),
+          validator: (value) {
+            if (value!.isEmpty) {
+              return "Please enter your password";
+            } else if (value.length < 6) {
+              return "Password must be at least 6 characters long";
+            }
+            return null;
+          },
+        ),
+        SizedBox(height: 80.h),
+        SizedBox(
+          height: 55.h,
+          width: double.infinity,
+          child: MaterialButton(
+            onPressed: () {
+              Navigator.pushNamedAndRemoveUntil(
+                context,
+                Routes.botNavBar,
+                (route) => false,
+              );
+            },
+            color: AppColor.primaryColor,
+            textColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.sp),
+            ),
+            child: Text("Login", style: AppTextStyle.poppins14BoldwhiteColor),
+          ),
+        ),
+        DoNotHaveAnAccount(
+          text: "Register",
+          onPressed:
+              () => Navigator.pushNamedAndRemoveUntil(
+                context,
+                '/register',
+                (route) => false,
+              ),
+        ),
+      ],
+    );
+  }
+}
