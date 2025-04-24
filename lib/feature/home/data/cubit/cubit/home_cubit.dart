d6d7f665f2632f1882a42bd8e584c98ba185d7f2
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-04-24 16:55:49 +0200

    add HomeCubit

--- lib/feature/home/data/cubit/cubit/home_cubit.dart
+++ lib/feature/home/data/cubit/cubit/home_cubit.dart
@@ -1,9 +1,23 @@
-import 'package:bloc/bloc.dart';
+import 'package:edunexus/core/helper/shared_pref_helper.dart';
 import 'package:edunexus/feature/home/data/model/all_courses.dart';
-import 'package:meta/meta.dart';
+import 'package:edunexus/feature/home/data/repos/all_courses_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
-  HomeCubit() : super(HomeInitial());
+  HomeCubit(this.allCoursesRepo) : super(HomeInitial());
+
+  final AllCoursesRepo allCoursesRepo;
+
+  Future<void> getAllCourses(String path) async {
+    emit(HomeLoading());
+
+    final token = await CacheHelper().getSecuredData(key: 'token') ?? '';
+    final result = await allCoursesRepo.getAllCourses(path, token);
+    result.fold(
+      (error) => emit(HomeError(errorMessage: error)),
+      (allCoursesModel) => emit(HomeSuccess(allCoursesModel: allCoursesModel)),
+    );
+  }
 }
