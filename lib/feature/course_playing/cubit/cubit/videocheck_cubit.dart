9f8abdd4f7087e3127ead7eff8b3faadb843ccb5
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-05-03 01:40:35 +0300

    VideocheckCubit

--- lib/feature/course_playing/cubit/cubit/videocheck_cubit.dart
+++ lib/feature/course_playing/cubit/cubit/videocheck_cubit.dart
@@ -1,9 +1,26 @@
-import 'package:bloc/bloc.dart';
+import 'package:edunexus/core/helper/app_constants.dart' show AppConstants;
+import 'package:edunexus/core/helper/shared_pref_helper.dart';
 import 'package:edunexus/feature/course_playing/data/models/video_check_model.dart';
-import 'package:meta/meta.dart';
+import 'package:edunexus/feature/course_playing/data/repos/video_check_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'videocheck_state.dart';
 
 class VideocheckCubit extends Cubit<VideocheckState> {
-  VideocheckCubit() : super(VideocheckInitial());
+  VideocheckCubit(this.videoCheckRepo) : super(VideocheckInitial());
+
+  final VideoCheckRepo videoCheckRepo;
+
+  Future<void> getAllLessons(String path) async {
+    emit(VideocheckLoading());
+
+    final token =
+        await CacheHelper().getSecuredData(key: AppConstants.token) ?? '';
+    final result = await videoCheckRepo.checkVideo(path, token);
+    result.fold((error) => emit(VideocheckError(message: error)), (
+      allcheckVideoModel,
+    ) {
+      emit(VideocheckLoaded(checkVideoModel: allcheckVideoModel));
+    });
+  }
 }
--- lib/feature/course_playing/cubit/cubit/videocheck_state.dart
+++ lib/feature/course_playing/cubit/cubit/videocheck_state.dart
@@ -1,6 +1,5 @@
 part of 'videocheck_cubit.dart';
 
-@immutable
 sealed class VideocheckState {}
 
 final class VideocheckInitial extends VideocheckState {}
