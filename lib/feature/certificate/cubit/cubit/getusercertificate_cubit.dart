92b004ff5467fb1b1735583609a1c66ea6224b48
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-05-04 21:46:45 +0300

    add GetusercertificateCubit

--- lib/feature/certificate/cubit/cubit/getusercertificate_cubit.dart
+++ lib/feature/certificate/cubit/cubit/getusercertificate_cubit.dart
@@ -1,9 +1,26 @@
-import 'package:bloc/bloc.dart';
+import 'package:edunexus/core/helper/app_constants.dart';
+import 'package:edunexus/core/helper/shared_pref_helper.dart';
 import 'package:edunexus/feature/certificate/data/model/user_certificate_model.dart';
-import 'package:meta/meta.dart';
+import 'package:edunexus/feature/certificate/data/repo/user_certificate_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'getusercertificate_state.dart';
 
 class GetusercertificateCubit extends Cubit<GetusercertificateState> {
-  GetusercertificateCubit() : super(GetusercertificateInitial());
+  GetusercertificateCubit(this.userCertificateRepo)
+    : super(GetusercertificateInitial());
+
+  final UserCertificateRepo userCertificateRepo;
+
+  Future<void> getUserCertificate(String path) async {
+    emit(GetusercertificateLoading());
+    final token = await CacheHelper().getSecuredData(key: AppConstants.token)??'';
+    final result = await userCertificateRepo.getAllLessons(path, token);
+    result.fold(
+      (error) => emit(GetusercertificateFailure(error: error)),
+      (userCertificateModel) => emit(
+        GetusercertificateSuccess(userCertificateModel: userCertificateModel),
+      ),
+    );
+  }
 }
